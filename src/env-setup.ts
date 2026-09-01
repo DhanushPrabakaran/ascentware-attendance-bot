928bdd3a4be5d6787c51d414a3c909981398a7fb
DhanushAscentware <dhanushprabakaran18@gmail.com>
2026-09-01 12:50:10 +0530

    Remove env-setup hack

--- src/env-setup.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-// Map legacy Bot Framework env vars to the new Agents SDK expected vars before any SDK modules load
-if (process.env.MicrosoftAppId && !process.env.CLIENT_ID) {
-    process.env.CLIENT_ID = process.env.MicrosoftAppId;
-}
-if (process.env.MicrosoftAppPassword && !process.env.CLIENT_SECRET) {
-    process.env.CLIENT_SECRET = process.env.MicrosoftAppPassword;
-}
